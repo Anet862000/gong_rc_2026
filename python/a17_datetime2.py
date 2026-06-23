783dafae0d2668c7501ba33afb1199d4fcd895c5
Anet862000 <younwoo1744@gmail.com>
2026-06-23 10:24:18 +0900

    season check

--- python/a17_datetime2.py
+++ python/a17_datetime2.py
@@ -10,5 +10,17 @@ def main():
     else:
         print(f"현재 시간은 {now.hour}시로 오후입니다.")
 
+
+    print(now.month, type(now.month))
+
+    if now.month == 12 or 1 <= now.month <= 3:
+        print("겨울")
+    elif 4 <= now.month <= 5:
+        print("봄")
+    elif 6 <= now.month <= 8:
+        print("여름")
+    else:
+        print("가을")
+
 if __name__ == "__main__":
     main()
