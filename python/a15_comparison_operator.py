445229e6cd743ecc631bac93600316b3d58a7584
Anet862000 <younwoo1744@gmail.com>
2026-06-23 09:53:17 +0900

    new

--- python/a15_comparison_operator.py
+++ python/a15_comparison_operator.py
@@ -2,7 +2,14 @@ def main():
     print(10 == 100)
     print(10 != 100)
     print(10 < 100)
-    print(10 <= 100)
+    print(100 <= 100)
+    print(type(True))
+
+    print(not True)
+    print(not False)
+    print(True and True)
+    print(True and False)
+    print(False and False)
 
 if __name__ == "__main__":
     main()
